10c6ae04d65fa4b0ebf0892867dd8ca20b3e3651
yuanjie-li <yuanjie.li@utexas.edu>
2018-06-27 17:19:45 -0700

    44

--- utils/genPrimes.py
+++ utils/genPrimes.py
@@ -1,7 +1,7 @@
 import utils
 import csv
 
-primeArr = utils.getPrimeArr(1000000000)
+primeArr = utils.getPrimeArr(100000000)
 
 with open('all_prime_list.csv', 'wb') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
